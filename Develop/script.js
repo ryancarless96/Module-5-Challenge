4909bc0e40e2abbb0a0f3b1b8011a742b81a74a9
Ryan Carless <ryancarless96@gmail.com>
2022-11-12 14:54:52 -0500

    commit changes

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 // in the html.
 var hourEl = $('#hour');
 var minuteEl = $('#minute');
-var secondEl =$('#second');
+var secondEl = $('#second');
 var dayEl = $('#day');
 
 
@@ -11,19 +11,19 @@ function daySchedule() {
     var time = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     hourEl.text(time);
 }
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    saveBtnEl.on("click",function (){
-for(var i=0; i < 5; i++) {
-  
-}
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+saveBtnEl.on("click", function () {
+    for (var i = 0; i < 5; i++) {
 
-    
-  
+    }
+
+
+
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -44,6 +44,6 @@ for(var i=0; i < 5; i++) {
     // TODO: Add code to display the current date in the header of the page.
     var currentDate = dayjs('2022-09-11')
     var today = dayjs();
-    var days = currentDate.diff(today,'day');
+    var days = currentDate.diff(today, 'day');
     $('a').text(days);
-  });
+});
