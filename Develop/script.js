ee96a777d29cd17d32511c406b8f30b6fb50c0c5
Ryan Carless <ryancarless96@gmail.com>
2022-11-13 14:07:37 -0500

    commit changes

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,11 @@ var minuteEl = $('#minute');
 var secondEl = $('#second');
 var dayEl = $('#day');
 
-
-function daySchedule() {
-    var time = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+function daySchedule(){
+    var time = moment().format('MMM D,YYYY');
     hourEl.text(time);
 }
+
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
